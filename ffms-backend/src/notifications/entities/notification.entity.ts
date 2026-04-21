574b2ca3be82263590aecf87dfdb8ebf4f91e5d0
bed-com-29-24 <bed-com-29-24@unima.ac.mw>
2026-04-21 13:28:01 -0700

    created the notification entity

--- ffms-backend/src/notifications/entities/notification.entity.ts
+++ ffms-backend/src/notifications/entities/notification.entity.ts
@@ -1,1 +1,24 @@
-export class Notification {}
+import { Farmer } from 'src/farmers/entities/farmer.entity';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity({name: 'NOTIFICATIONS'})
+export class Notification {
+  @PrimaryGeneratedColumn({name: 'NOTIFICATION_ID'})
+  notificationId: number;
+
+  @ManyToOne(() => Farmer, { eager: false })
+  @JoinColumn({ name: 'FARMER_ID' })
+  farmer: Farmer;
+
+  @Column()
+  title: string;
+
+  @Column({name: 'MESSAGE', length: 1000})
+  message: string;
+
+  @Column({name: 'TYPE', length: 30})
+  type: 'budget alert' | 'reminder' | 'system';
+
+  @Column({name: 'IS_READ', type: 'number', default: 0})
+    isRead: number;
+}
