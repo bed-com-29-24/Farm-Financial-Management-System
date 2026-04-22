c90c6135e5db40d4960df3c661f8e632e0ab0842
Ulemu Namale <bsc-inf-01-24@unima.ac.mw>
2026-04-22 12:49:31 +0200

    Fixing The auth structure

--- ffms-backend/ffms-backend/src/app.module.ts
+++ ffms-backend/ffms-backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FarmersModule } from './farmers/farmers.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [FarmersModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
