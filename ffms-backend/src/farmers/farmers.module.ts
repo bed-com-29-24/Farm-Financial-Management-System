48e7064d9ea9ae812207c4cc9b1171eb8582a82a
Ulemu Namale <bsc-inf-01-24@unima.ac.mw>
2026-04-22 13:22:36 +0200

    a fix structure push

--- ffms-backend/src/farmers/farmers.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FarmersController } from './farmers.controller';
-import { FarmersService } from './farmers.service';
-
-@Module({
-  controllers: [FarmersController],
-  providers: [FarmersService]
-})
-export class FarmersModule {}
