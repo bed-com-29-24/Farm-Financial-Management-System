54f75bbcbb44e7dba872259b90d18cc7f8f7194a
bsc-48-24tiya <bsc-48-24@unima.ac.mw>
2026-04-28 05:20:01 +0200

    Created farmers and categories

--- ffms-backend/src/farmers/farmers.service.ts
+++ ffms-backend/src/farmers/farmers.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Farmer } from './entities/farmer.entity';
@@ -26,6 +26,12 @@ export class FarmersService {
     return this.farmersRepo.findOne({
       where: { farmerId: id },
     });
+  }
+
+  async findById(id: number): Promise<Farmer> {
+    const farmer = await this.findOne(id);
+    if (!farmer) throw new NotFoundException(`Farmer ${id} not found`);
+    return farmer;
   }
 
   // UPDATE
